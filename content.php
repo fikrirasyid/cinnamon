e253fcc0f04b73f3527288141d072e551c992b85
Fikri Rasyid <fikrirasyid@gmail.com>
2014-11-09 07:04:27 +0700

    Adding featured image as background
    Featured image is displayed as background on stream (index, archive,
    search)

--- content.php
+++ content.php
@@ -5,6 +5,14 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( has_post_thumbnail() ) : ?>
+
+		<div class="entry-featured-image as-background">
+			<?php the_post_thumbnail( 'full' ); ?>			
+		</div>
+
+	<?php endif; ?>
+
 	<header class="entry-header">
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
