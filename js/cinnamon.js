2dbccc2aaebd37cbab12b5fd7fc44e5590c73779
Fikri Rasyid <fikrirasyid@gmail.com>
2014-12-22 07:45:48 +0700

    Civil Footnotes support
    Sliding the window instead of jumping when user clicking footnotes’ link

--- js/cinnamon.js
+++ js/cinnamon.js
@@ -17,6 +17,20 @@ jQuery(document).ready(function($) {
 			$(this).removeClass( 'hovered' );
 		}
 	);
+
+	/**
+	* Civil Footnotes Support
+	* Slide the window instead of jumping it
+	*/
+	$('#main').on( 'click', 'a[rel="footnote"], a.backlink', function(e){
+		e.preventDefault();
+		var target_id = $(this).attr('href');
+		var respond_offset = $(target_id).offset();
+
+		$('html, body').animate({
+			scrollTop : respond_offset.top - 90
+		});
+	});		
 });
 
 /**
